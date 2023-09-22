6fed91e599861e784bd0bdb8bfa5b7467739ea68
Douglas Lessa <dougstefe@gmail.com>
2023-09-22 15:50:23 -0300

    feat: map account login route

--- src/Edc.Api/Extensions/AccountContextExtension.cs
+++ src/Edc.Api/Extensions/AccountContextExtension.cs
@@ -31,6 +31,17 @@ public static class AccountContextExtension {
             Core.AccountContext.UseCases.Create.Validation
         >();
         #endregion
+
+        #region Login
+        builder.Services.AddTransient<
+            Core.AccountContext.UseCases.Login.Interfaces.IGetRepository,
+            Infra.AccountContext.UseCases.Login.Repository
+        >();
+        builder.Services.AddScoped<
+            Core.AccountContext.UseCases.Login.Interfaces.IValidation,
+            Core.AccountContext.UseCases.Login.Validation
+        >();
+        #endregion
     }
 
     public static void MapAccountRoutes(this WebApplication app) {
@@ -51,6 +62,51 @@ public static class AccountContextExtension {
                 }
 
                 return Results.Json(data: response.Data, statusCode: StatusCodes.Status201Created);
+            }
+        );
+
+        app.MapGet("api/v1/products",
+            () => {
+                var products = new [] {new { Id = 1 }, new {Id = 2}};
+                return Results.Json(data: products, statusCode: StatusCodes.Status200OK);
+            }
+        ).RequireAuthorization("User");
+
+        app.MapPost("api/v1/products",
+            () => {
+                var product = new { Id = 3};
+                return Results.Json(data: product, statusCode: StatusCodes.Status201Created);
+            }
+        ).RequireAuthorization("Admin");
+        
+        app.MapPost(
+            "api/v1/login",
+            async (
+                Notification _notification,
+                Core.AccountContext.UseCases.Login.Request request,
+                IRequestHandler<
+                    Core.AccountContext.UseCases.Login.Request,
+                    Core.SharedContext.UseCases.Response<Core.AccountContext.UseCases.Login.ResponseData>
+                > handler
+            ) => {
+                var response = await handler.Handle(request, new CancellationToken());
+
+                if(!response.IsSuccess) {
+                    return _notification.GetResult();
+                }
+
+                var accessToken = AccessTokenExtension.GenerateToken(response.Data);
+                return Results.Json(
+                    data: new {
+                        accessToken,
+                        response.Data.Id,
+                        response.Data.Name,
+                        response.Data.Email,
+                        response.Data.Roles
+                    },
+                    statusCode: StatusCodes.Status200OK
+                );
+
             }
         );
     }
