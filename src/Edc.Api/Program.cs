f0c2de50f18dc0d40114e61cbfb50996b6033335
Douglas Lessa <dougstefe@gmail.com>
2023-09-22 15:51:08 -0300

    feat: config builder and app

--- src/Edc.Api/Program.cs
+++ src/Edc.Api/Program.cs
@@ -1,11 +1,23 @@
+using Edc.Api.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.AddConfiguration();
+builder.AddDbContext();
+builder.AddAuthentication();
+
+builder.AddAccountContext();
+
+builder.AddMediatR();
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapAccountRoutes();
 
 app.Run();
