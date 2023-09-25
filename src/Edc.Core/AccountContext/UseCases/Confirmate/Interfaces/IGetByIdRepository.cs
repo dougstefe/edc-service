943a9805e1bc7a401e9575babd314edb1927e7e4
Douglas Lessa <dougstefe@gmail.com>
2023-09-25 17:31:19 -0300

    feat: implement confirmation repository

--- src/Edc.Core/AccountContext/UseCases/Confirmate/Interfaces/IGetByIdRepository.cs
+++ src/Edc.Core/AccountContext/UseCases/Confirmate/Interfaces/IGetByIdRepository.cs
@@ -1,5 +1,7 @@
 using Edc.Core.AccountContext.Entities;
 
+namespace Edc.Core.AccountContext.UseCases.Confirmate.Interfaces;
+
 public interface IGetByIdRepository {
-    Task<Account> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+    Task<Account?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
 }
